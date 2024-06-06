1c73755d822aa510812de2d6b35f89fced485b53
Michał Dziechciarz <225484@buas.nl>
2024-06-06 17:22:59 +0200

    Add coach profile redirection on CoachCard click

--- app/dashboard/coaches/components/CoachCard/CoachCard.js
+++ app/dashboard/coaches/components/CoachCard/CoachCard.js
@@ -1,11 +1,18 @@
 import {Call16Filled, Mail24Filled} from '@fluentui/react-icons';
 import {Chip} from '@nextui-org/react';
 import Image from 'next/image';
+import {useRouter} from 'next/navigation';
 import styles from './CoachCard.module.css';
 
 const CoachCard = ({name, groups = [], imgSrc, phoneNumber, email}) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/dashboard/coaches/1');
+  };
+
   return (
-    <li className={styles.container}>
+    <li className={styles.container} onClick={handleClick}>
       <div className={styles.avatarWrapper}>
         <Image className={styles.avatar} src={imgSrc} alt="Avatar" width={300} height={300} />
       </div>
